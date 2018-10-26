03524e5357072209accc863ea0a223b7784abb02
Hyung Lee <hyungnaelee@gmail.com>
2018-10-26 15:01:28 -0700

    Add grid system for featured items list.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
 import { HeaderAdvertComponent } from './header-advert/header-advert.component';
 import { MainCarouselComponent } from './main-carousel/main-carousel.component';
+import { FeaturedListComponent } from './featured-list/featured-list.component';
+import { FeaturedItemComponent } from './featured-item/featured-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderBarComponent,
     HeaderAdvertComponent,
-    MainCarouselComponent
+    MainCarouselComponent,
+    FeaturedListComponent,
+    FeaturedItemComponent
   ],
   imports: [
     BrowserModule,
