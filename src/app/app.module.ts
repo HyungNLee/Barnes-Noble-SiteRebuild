f6123b5fcc1466067ed552ea376ff73ae78086d8
Hyung Lee <hyungnaelee@gmail.com>
2018-10-26 09:11:30 -0700

    Create right side of header-bar-one with links included,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent
   ],
   imports: [
     BrowserModule,
